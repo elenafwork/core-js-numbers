91eff8712915d325752b6543ecf86710a7f5ef44
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2024-01-06 14:06:00 -0600

    fix: toString, toExponential

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -370,8 +370,8 @@ function getSine(num) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  return 0;
+function numberToStringInBase(number, base) {
+  return number.toString(base);
 }
 
 /**
@@ -384,8 +384,8 @@ function numberToStringInBase(/* number, base */) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  return 0;
+function toExponential(number, fractionDigits) {
+  return number.toExponential(fractionDigits).toString();
 }
 
 /**
